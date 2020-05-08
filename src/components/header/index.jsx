11d2e0b65145866493aa3ff2d2ccf81a35df4d40
Glushenko.Vitaliy <vts.ghlushenko@gmail.com>
2020-05-08 12:05:41 +0300

    layout header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,23 @@ import styles from './header.module.scss';
 export const Header = () => {
   return (
     <header className={styles.header}>
-      Welcome to the <span className={styles.name}>RedBook live</span>
+      <span className={styles.name}>
+        <span>
+          Red
+        </span>
+        Book live
+      </span>
+      <div className={styles.navigation}>
+        <span>Про нас</span>
+        <span>Проект</span>
+        <span>Організації</span>
+        <span>Важливі Ділянки</span>
+        <span>Контакти</span>
+        <span className={styles.change__lang}>
+          <span>UA</span>
+          <span className={styles.arrow}></span>
+        </span>
+      </div>
     </header>
   )
 }
