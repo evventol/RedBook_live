aacf0f6fcea6394eb12d3a5711439c99e0a9acdc
Stratienko <artemstratienko94@gmail.com>
2020-05-14 18:47:56 +0300

    Fixed the footer markup

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,36 +1,34 @@
 import React from 'react';
 import styles from './footer.module.scss';
-import faceBook from '../../img/facebook.svg';
+import facebook from '../../img/facebook.svg';
 import instagram from '../../img/instagram.svg';
-import youTuBe from '../../img/youtube.svg';
+import youtube from '../../img/youtube.svg';
 import copyrightLogo from '../../img/copyright.svg';
 
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.name__with__copyright}>
-        <span className={styles.name}>RedBook live</span>
-        <div className={styles.copyright}>
-          <span className={styles.logo}><img src={copyrightLogo} alt=""/></span>
-          <span className={styles.text}>Copyright</span>
-        </div>
-      </div>
-
-      <nav>
-        <ul className={styles.navigation}>
+      <div>
+        <span className={styles.title}>
+          RedBook live
+        </span>
+        <ul className={styles.tabs}>
           <li className={styles.tab}>Про Нас</li>
           <li className={styles.tab}>Проект</li>
           <li className={styles.tab}>Організації</li>
           <li className={styles.tab}>Важливі Ділянки</li>
           <li className={styles.tab}>Контакти</li>
         </ul>
-      </nav>
-
-      <div className={styles.social__net}>
-        <span className={styles.logo}><img src={faceBook} alt=""/></span>
-        <span className={styles.logo}><img src={instagram} alt=""/></span>
-        <span className={styles.logo}><img src={youTuBe} alt=""/></span>
+        <div className={styles.links}>
+          <img className={styles.link} src={facebook} alt="link"/>
+          <img className={styles.link} src={instagram} alt="link"/>
+          <img className={styles.link} src={youtube} alt="link"/>
+        </div>
+      </div>
+      <div>
+        <img src={copyrightLogo} alt=""/>
+        <span className={styles.text}>Copyright</span>
       </div>
     </footer>
   )
