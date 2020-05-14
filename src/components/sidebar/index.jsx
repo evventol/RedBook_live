c33fa8cac359e3ca5f9dcc64dd4e45da43501a27
Artem Stratienko <33661783+Stratienko@users.noreply.github.com>
2020-05-14 21:33:14 +0300

    Created collapsable sidebar (#11)

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -6,18 +6,25 @@ import SelectRegion from './selectRegion'
 import SelectOTG from './selectOTG'
 import SelectDataRegister from './selectDateRegister'
 import PositioningError from './positioningError'
+import arrow from '../../img/sidebar_arrow.svg';
 
 export default function Sidebar(props) {
   const [isOpened, toggleSidebar] = useState(false);
 
   return (
     <div className={[styles.sidebar, !isOpened ? styles.opened : ''].join(' ')}>
+      <div className={styles.toggler} onClick={() => toggleSidebar(!isOpened)}>
+        <img className={[styles.arrow, !isOpened ? '' : styles.closed].join(' ')} src={arrow} alt="toggler"/>
+      </div>
+      <div className={styles.content}>
         <Search />
         <SelectTaxon selectTaxonData={props.selectTaxonData} />
         <SelectRegion />
         <SelectOTG />
         <SelectDataRegister />
         <PositioningError positioningErrorData={props.positioningErrorData} />
+      </div>
     </div>
+    
   )
 }
